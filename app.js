1a352800585ccabc494edf592d3ab3f2ab5175f5
goog34 <goog34@naver.com>
2016-12-21 18:35:42 +0900

    ejs edit

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.set("view engine",'ejs');
+//app.use(express.static(__dirname + '/public'));
 
+/*
+app.get('/',function(request, response){
+    response.render('my_first_ejs');
+  }
+);
+*/
+/*
 app.listen(3000,function(){
     console.log('Server On!!');
   }
 );
+*/
