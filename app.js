e8028829daaff7f7855c122264b0d44f8e563867
goog34 <goog34@naver.com>
2016-12-21 17:47:48 +0900

    Css Plus

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/',function(request, response){
-      response.send('Hello World');
-  }
-);
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000,function(){
     console.log('Server On!!');
